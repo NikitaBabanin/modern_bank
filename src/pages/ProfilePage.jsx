3dabb2a91ab3cc2a5266d2ba31f9b1851b2e2afd
NikitaBabanin <postmanngmilus@gmail.com>
2023-01-07 12:10:21 -0800

    Created empty components for Profile page.

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import { Profile } from '../components'
+
+const ProfilePage = () =>{
+    return(
+        <div>
+            <h1>Profile page</h1>
+            <Profile />
+        </div>
+    )
+}
+export default ProfilePage
