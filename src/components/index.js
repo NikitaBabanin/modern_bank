e00ff6d01ccdee3709279f806df159c3f9ea98a3
NikitaBabanin <postmanngmilus@gmail.com>
2023-01-07 12:12:33 -0800

    Changed project file structure.

--- src/components/index.js
+++ src/components/index.js
@@ -1,16 +1,19 @@
-import Navbar from "./Navbar";
-import Billing from "./Billing";
-import Business from "./Business";
+import Navbar from "./Navbar.jsx";
+import Billing from "./Home/Billing.jsx";
+import Business from "./Home/Business.jsx";
 import Button from "./Button";
-import CardDeal from "./CardDeal";
-import Clients from "./Clients";
-import CTA from "./CTA";
-import FeedbackCard from "./FeedbackCard";
-import Footer from "./Footer";
-import GetStarted from "./GetStarted";
-import Hero from "./Hero";
-import Stats from "./Stats";
-import Testimonials from "./Testimonials";
+import CardDeal from "./Home/CardDeal.jsx";
+import Clients from "./Home/Clients.jsx";
+import CTA from "./Home/CTA.jsx";
+import FeedbackCard from "./Home/FeedbackCard.jsx";
+import Footer from "./Home/Footer.jsx";
+import GetStarted from "./Home/GetStarted.jsx";
+import Hero from "./Home/Hero.jsx";
+import Stats from "./Home/Stats.jsx";
+import Testimonials from "./Home/Testimonials.jsx";
+import Login from './Auth/Login.jsx';
+import Signup from './Auth/Signup.jsx';
+import Profile from './Profile.jsx';
 
 export {
     Navbar,
@@ -25,5 +28,8 @@ export {
     GetStarted,
     Hero,
     Stats,
-    Testimonials
+    Testimonials,
+    Login,
+    Signup,
+    Profile
 }
