314155d76477863272a2ad2baed09aab76d98069
NikitaBabanin <postmanngmilus@gmail.com>
2023-01-14 00:43:48 -0800

    feat(husky): configured

--- src/components/Auth/test/Login.test.js
+++ src/components/Auth/test/Login.test.js
@@ -1,7 +1,6 @@
-
 describe('Fake test', () => {
-    it('Login fake test', async () => {
-        const fakeFunc = (x,y) => x * y
-        expect(fakeFunc(2,7)).toBe(14);
-    });
+  it('Login fake test', async () => {
+    const fakeFunc = (x, y) => x * y;
+    expect(fakeFunc(2, 7)).toBe(14);
+  });
 });
